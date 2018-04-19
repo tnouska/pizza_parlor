d9e3d9df57dbbef52420744fe1100c2ab3fe9d90
Teagan Nouska <tnouska@live.com>
2018-04-19 11:18:57 -0500

    post to order table added

--- server/routes/order.router.js
+++ server/routes/order.router.js
@@ -5,12 +5,26 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
     const queryText = 'SELECT * FROM "order"'
-    pool.query(queryText).then((response) => {
+    pool.query(queryText)
+    .then((response) => {
         res.send(response.rows);
-    }).catch((error) => {
+    })//end .then
+    .catch((error) => {
         console.log('error in pizza.router.get: ', error);
         res.sendStatus(500)
     });//end pool.query to get all orders from order table
 });//end router.get
 
+router.post('/', (req,res)=>{
+    let newOrder = req.body;
+    const queryText = `INSERT INTO order (customer_name,order_total)`
+    pool.query(queryText, [req.body.customer_name, req.body.order_total])
+    .then((response)=>{
+        res.sendStatus(201);
+    })//end .then
+    .catch((error)=>{
+        console.log('error inside order.router.post: ', error);
+        res.sendStatus(500);
+    });//end pool.query to add to order table
+});//end router.post
 module.exports = router;
