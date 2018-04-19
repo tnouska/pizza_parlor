1bf9711dfbb5e2fccc1d8821ff7a5dc23e2aa8f4
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-04-19 10:09:37 -0500

    completed initial imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { call, put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+import App from './components/App/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
