402ff020a5a780eb5cf5f5eedb9693176b70d499
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-04-19 10:22:18 -0500

    added reducers, sagas for pizzaList

--- src/index.js
+++ src/index.js
@@ -9,7 +9,44 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
-import App from './components/App/App'
+import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//create Saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+function* rootSaga(){
+    yield takeEvery('FETCH_PIZZA', getPizzaSaga)
+}
+
+function* getPizzaSaga( action ){
+    try{
+        const pizzaResponse = yield call(axios.get, '/pizza');
+        yield put({
+            type: 'SET_PIZZA',
+            payload: pizzaResponse.data
+        })
+    } catch ( error ) {
+
+    }
+}
+
+const pizzaList = (state = [], action) => {
+    switch(action.type) {
+        case 'SET_PIZZA': 
+            return action.payload
+        default:
+            return state
+    }
+}
+
+//create store
+const store = createStore (
+    combineReducers({ pizzaList }),
+    applyMiddleware(sagaMiddleware, logger)
+)
+
+sagaMiddleWare.run(rootSaga);
+
+ReactDOM.render(<provider store={store}><App /></ provider>, document.getElementById('root'));
 registerServiceWorker();
