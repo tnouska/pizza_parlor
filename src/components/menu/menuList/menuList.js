f597f5129ba3b262723655fc11a3320147b3169a
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-04-19 16:45:22 -0500

    moved to menuListItem and increment and decremnt functioning

--- src/components/menu/menuList/menuList.js
+++ src/components/menu/menuList/menuList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import IconButton from 'material-ui/IconButton';
-import { AddCircle, RemoveCircle } from 'material-ui-icons';
+import MenuListItem from './menuListItem.js'
 
 const mapStateToProps = reduxState => ({
     reduxState,
@@ -14,34 +13,11 @@ class MenuList extends Component {
         })
     }
 
-    addOnePizza = () =>{
-        const addPizza = this.props.reduxState.updateQuantity +1;
-        this.props.dispatch({
-            type: "UPDATE_QUANTITY",
-            payload: addPizza
-        })
-    }
-
-    deleteOnePizza = () => {
-        const deletePizza = this.props.reduxState.updateQuantity - 1;
-        this.props.dispatch({
-            type: "UPDATE_QUANTITY",
-            payload: deletePizza
-        })
-    }
-
     render() {
 
         //map out pizza
         let allPizzaItems = this.props.reduxState.pizzaList.map((pizza) => {
-            return <div className="pizzaDiv" key={pizza.id}>
-                <h3>{pizza.name}</h3>
-                <p>{pizza.description}</p>
-                <h4>${pizza.cost}</h4>
-               <IconButton size="small" color="secondary" onClick={this.addOnePizza}><AddCircle /></IconButton>
-                {this.props.reduxState.updateQuantity}
-                <IconButton size="small" color="secondary" onClick={this.deleteOnePizza}><RemoveCircle /></IconButton>
-            </div>
+            return <MenuListItem key={pizza.id} pizza={pizza} />
         })
         return (
             <div> {allPizzaItems} </div>
---
+++ src/components/menu/menuList/menuListItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import IconButton from 'material-ui/IconButton';
+import { AddCircle, RemoveCircle } from 'material-ui-icons';
+
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
+class MenuListItem extends Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            quantity: 0
+        }
+    }//end constructor
+
+    addOnePizza = () => {
+        // const addPizza = this.setState.quantity + 1;
+        this.setState({
+            quantity: this.state.quantity + 1
+        })
+        this.props.dispatch({
+            type: "UPDATE_QUANTITY",
+            payload: this.state.quantity
+        })        
+    }
+
+    deleteOnePizza = () => {
+        // const deletePizza = this.setState.quantity - 1;
+        this.setState({
+            quantity: this.state.quantity - 1
+        })
+        this.props.dispatch({
+            type: "UPDATE_QUANTITY",
+            payload: this.state.quantity
+        })
+
+    }
+
+    render(){
+
+        return (
+            <div className = "pizzaDiv">
+                <h3>{this.props.pizza.name}</h3>
+                <p>{this.props.pizza.description}</p>
+                <h4>${this.props.pizza.cost}</h4>
+            <IconButton size="small" color="secondary" onClick={this.addOnePizza}><AddCircle /></IconButton>
+            <span> {this.state.quantity}</span>
+            <IconButton size="small" color="secondary" onClick={this.deleteOnePizza}><RemoveCircle /></IconButton>
+            </div>
+        )
+        
+    }//render
+}//end class
+
+export default connect(mapStateToProps)(MenuListItem);
